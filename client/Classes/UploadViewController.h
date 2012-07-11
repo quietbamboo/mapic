0ef5a4eb20dfacba233f9c6f613538e9041c861d
josephgao <gaojf.bokecc@gmail.com>
2012-07-11 09:32:18 +0800

    add uploadview controller

---
+++ client/Classes/UploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  UploadViewController.h
+//  template
+//
+//  Created by yujie liu on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadViewController : UIViewController
+
+@end
---
+++ client/Classes/UploadViewController.m
@@ -0,0 +1,50 @@
+//
+//  UploadViewController.m
+//  template
+//
+//  Created by yujie liu on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UploadViewController.h"
+
+@interface UploadViewController ()
+
+@end
+
+@implementation UploadViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)loadView {
+    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0,0, 320, 480)];
+    contentView.backgroundColor = [UIColor whiteColor];
+    self.view = contentView;
+    [contentView release];
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
