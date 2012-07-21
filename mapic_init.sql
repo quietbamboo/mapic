6ca3e3a0b634cba23a86a6b78ad068b425e91087
josephgao <gaojf.bokecc@gmail.com>
2012-07-21 10:23:02 +0800

    add database init sql

---
+++ mapic_init.sql
@@ -0,0 +1,140 @@
+/*
+SQLyog Ultimate v8.63 
+MySQL - 5.5.15 : Database - mapic
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mapic` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `mapic`;
+
+/*Table structure for table `comment` */
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `pictureId` int(18) DEFAULT NULL,
+  `commentType` int(4) DEFAULT NULL COMMENT '1:like 2:dislike 3:report 4:comment',
+  `userId` int(18) DEFAULT NULL,
+  `creationTime` datetime DEFAULT NULL,
+  `commentContent` mediumtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `comment` */
+
+/*Table structure for table `picture` */
+
+DROP TABLE IF EXISTS `picture`;
+
+CREATE TABLE `picture` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `groupId` int(18) DEFAULT NULL,
+  `url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deleteFlag` int(4) DEFAULT NULL,
+  `likes` int(255) DEFAULT NULL,
+  `dislikes` int(255) DEFAULT NULL,
+  `report` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `picture` */
+
+/*Table structure for table `picturegroup` */
+
+DROP TABLE IF EXISTS `picturegroup`;
+
+CREATE TABLE `picturegroup` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `userId` int(18) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` mediumtext COLLATE utf8_unicode_ci,
+  `creationTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `picturegroup` */
+
+/*Table structure for table `relationship` */
+
+DROP TABLE IF EXISTS `relationship`;
+
+CREATE TABLE `relationship` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `followerId` int(18) DEFAULT NULL,
+  `followId` int(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `relationship` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `loginName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `creationTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user` */
+
+/*Table structure for table `userinfo_basic` */
+
+DROP TABLE IF EXISTS `userinfo_basic`;
+
+CREATE TABLE `userinfo_basic` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `userId` int(18) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mobileNumber` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `homeNumber` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sex` varchar(4) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `follows` int(255) DEFAULT NULL,
+  `followed` int(255) DEFAULT NULL,
+  `signature` mediumtext COLLATE utf8_unicode_ci,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `userinfo_basic` */
+
+/*Table structure for table `userinfohistory` */
+
+DROP TABLE IF EXISTS `userinfohistory`;
+
+CREATE TABLE `userinfohistory` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` mediumtext COLLATE utf8_unicode_ci,
+  `creationTime` datetime DEFAULT NULL,
+  `userId` int(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `userinfohistory` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
